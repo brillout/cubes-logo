ce61cb1fb6ab8eae42f851bc5c651bb0dc30db70
Romuald Brillout <git@brillout.com>
2022-03-10 12:31:59 +0100

    wip

--- main.js
+++ main.js
@@ -9,12 +9,10 @@ var delayBeforeAnimation = 5000
 
 let illo = new Zdog.Illustration({
   element: '.zdog-canvas',
-  dragRotate: true
-  // onDragStart: function() { isSpinning = false; },
-  // onDragStop: function() { isSpinning = true; },
+  dragRotate: true,
+  onDragStart: function() { isSpinning = false; },
+  onDragStop: function() { isSpinning = true; },
 })
-
-var boxes = []
 
 // top of the box
 let box1 = new Zdog.Box({
@@ -62,70 +60,14 @@ function makeStrokeBox(color, i) {
   orgbox.copy({ stroke: boxStroke, fill: false, color: c })
 }
 
-// animation
-var TAU = Zdog.TAU
-var ticker = 0
-
 function animate() {
   // update
   if (isSpinning) {
     // animateRoteSlice();
-    //annimateRotateAll();
+    // annimateRotateAll();
   }
   illo.updateRenderGraph()
   requestAnimationFrame(animate)
-}
-
-var animationSpeed = TAU / 4 / 30
-var delayBetweenAnimation = 250
-
-var ticker2 = 0
-function animateRoteSlice() {
-  box1.rotate.z += isSpinning ? animationSpeed : 0
-  ticker++
-  if (ticker >= TAU / 4 / animationSpeed) {
-    isSpinning = false
-    ticker = 0
-
-    ticker2++
-
-    if (ticker2 == 4) {
-      ticker2 = 0
-      box1.rotate.z = 0
-      setTimeout(function () {
-        isSpinning = true
-      }, delayBeforeAnimation)
-    } else {
-      setTimeout(function () {
-        isSpinning = true
-      }, delayBetweenAnimation)
-    }
-  }
-}
-
-var cycleCount = 105
-
-var keyframes = [
-  { x: 1.0, y: 0.0 },
-  { x: TAU * 0.1, y: TAU * 0.3 },
-  { x: TAU * -0.1, y: TAU * 1.4 },
-  { x: TAU * -0.3, y: TAU * 0.8 },
-  { x: -0.7, y: 0.2 },
-  { x: TAU * -0.1, y: TAU * 1.2 },
-  { x: TAU * 0.1, y: TAU * 1.3 },
-  { x: 1.0, y: 0.0 }
-]
-
-function annimateRotateAll() {
-  var progress = ticker / cycleCount
-  var tween = Zdog.easeInOut(progress % 1, 4)
-  var turnLimit = keyframes.length - 1
-  var turn = Math.floor(progress % turnLimit)
-  var keyA = keyframes[turn]
-  var keyB = keyframes[turn + 1]
-  illo.rotate.x = Zdog.lerp(keyA.x, keyB.x, tween)
-  illo.rotate.y = Zdog.lerp(keyA.y, keyB.y, tween)
-  ticker++
 }
 
 illo.translate.y = -50
@@ -135,7 +77,7 @@ illo.rotate.z = 0.8
 illo.rotate.x = 1.0
 
 illo.updateRenderGraph()
-let isSpinning = false
+let isSpinning = true
 animate()
 setTimeout(function () {
   isSpinning = true
